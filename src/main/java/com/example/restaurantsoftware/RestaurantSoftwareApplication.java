c4f30359263c2d0f308a48fb0ee3caa50f69d1c8
Admin <professormad966974@gmail.com>
2023-04-24 17:20:16 +0300

    Starting

---
+++ src/main/java/com/example/restaurantsoftware/RestaurantSoftwareApplication.java
@@ -0,0 +1,13 @@
+package com.example.restaurantsoftware;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestaurantSoftwareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestaurantSoftwareApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/restaurantsoftware/RestaurantSoftwareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.restaurantsoftware;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestaurantSoftwareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
